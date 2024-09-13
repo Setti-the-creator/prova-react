ac883046377cf048b9b5bb30c584f74677431766
setti-the-creator <settiwarrior@gmail.com>
2024-09-13 19:52:51 -0300

    feat:Header e footer

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+export default function Header() {
+    return (
+        <>
+        <header id='cabecalho'>
+            <h1>Seja bem-vindo </h1>
+        </header>
+        </>
+    )
+}
