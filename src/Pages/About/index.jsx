88b80a81a481e2efc43723659c84ad003828e741
Thiago Zanin <thzanin@gmail.com>
2024-09-13 20:41:14 -0300

    feat: Footer; About page

---
+++ src/Pages/About/index.jsx
@@ -0,0 +1,23 @@
+import './styles.css'
+
+export default function About(){
+    return(
+        <div id="about">
+            <section>
+                <h2>Sobre o site</h2>
+                <span>Neste site você poderá usar a api do Chuck Norris que gera algumas piadas para você (em inglês).</span>
+            </section>
+            <section>
+                <h2>Sobre o Chuck Norris</h2>
+                <ul>
+                    <li><bold>Nome completo:</bold> Carlos Ray "Chuck" Norris</li>
+                    <li><bold>Data de nascimento:</bold> 10 de março de 1940</li>
+                    <li><bold>Local de nascimento:</bold> Ryan, Oklahoma, EUA</li>
+                </ul>
+                <span>Chuck Norris é um renomado ator, artista marcial e escritor americano. Nascido em Ryan, Oklahoma, Norris começou sua carreira no mundo das artes marciais, onde se destacou como campeão mundial de karatê. Sua trajetória no cinema começou na década de 1970, com destaque para o filme "Way of the Dragon" (1972), onde enfrentou Bruce Lee em uma lendária luta.</span>
+                <p><span>Norris alcançou fama internacional com a série de televisão "Walker, Texas Ranger", que foi exibida de 1993 a 2001. A série consolidou sua imagem como um herói durão e íntegro, e tornou-o um ícone da cultura popular.</span></p>
+                <p><span>Além de seu trabalho como ator, Chuck Norris é também um autor prolífico, tendo escrito vários livros sobre artes marciais, filosofia e temas políticos. Ele é conhecido por seu envolvimento em várias causas filantrópicas e tem uma presença marcante na cultura de memes da internet, onde suas façanhas fictícias são amplamente celebradas.</span></p>                
+            </section>
+        </div>
+    )
+}
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,8 @@ import './styles.css'
 
 export default function Footer() {
     return (
-        <>
-        </>
+        <footer>
+            &copy; 2024 - Thiago Zanin e Victor Paulo Setti
+        </footer>
     )
 }
