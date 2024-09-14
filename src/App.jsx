bcc886af03ec599d8ca2c3e9893cdf55b8db27c6
Thiago Zanin <thzanin@gmail.com>
2024-09-13 21:06:22 -0300

    feat: Estilização about page (incompleta)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
 import Home from './Pages/Home'
+import About from './Pages/About'
 
 export default function App() {
  
@@ -11,8 +12,8 @@ export default function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home/>}/>
-        {/* <Route path='/about' element={<About/>}/>
-        <Route path='/jokes' element={<Jokes/>}/> */}
+        <Route path='/about' element={<About/>}/>
+        {/* <Route path='/jokes' element={<Jokes/>}/> */}
       </Routes>
     </>
   )
--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -3,16 +3,13 @@ import './styles.css'
 export default function About(){
     return(
         <div id="about">
-            <section>
-                <h2>Sobre o site</h2>
-                <span>Neste site você poderá usar a api do Chuck Norris que gera algumas piadas para você (em inglês).</span>
-            </section>
-            <section>
-                <h2>Sobre o Chuck Norris</h2>
+            <img class="imagem" width={220} height={300} src="//upload.wikimedia.org/wikipedia/commons/thumb/3/30/Chuck_Norris_May_2015.jpg/220px-Chuck_Norris_May_2015.jpg"/>
+            <section id="sessao">
+                <h2 id="subtitulo-about">Sobre o Chuck Norris</h2>
                 <ul>
-                    <li><bold>Nome completo:</bold> Carlos Ray "Chuck" Norris</li>
-                    <li><bold>Data de nascimento:</bold> 10 de março de 1940</li>
-                    <li><bold>Local de nascimento:</bold> Ryan, Oklahoma, EUA</li>
+                    <li><strong>Nome completo:</strong> Carlos Ray "Chuck" Norris</li>
+                    <li><strong>Data de nascimento:</strong> 10 de março de 1940</li>
+                    <li><strong>Local de nascimento:</strong> Ryan, Oklahoma, EUA</li>
                 </ul>
                 <span>Chuck Norris é um renomado ator, artista marcial e escritor americano. Nascido em Ryan, Oklahoma, Norris começou sua carreira no mundo das artes marciais, onde se destacou como campeão mundial de karatê. Sua trajetória no cinema começou na década de 1970, com destaque para o filme "Way of the Dragon" (1972), onde enfrentou Bruce Lee em uma lendária luta.</span>
                 <p><span>Norris alcançou fama internacional com a série de televisão "Walker, Texas Ranger", que foi exibida de 1993 a 2001. A série consolidou sua imagem como um herói durão e íntegro, e tornou-o um ícone da cultura popular.</span></p>
